87ba0def2eef6f432a5c7655680551917a135fe2
Saad Aldosary <47954648+saadaldosary@users.noreply.github.com>
2022-12-17 13:57:00 -0500

    v2

--- wg_install.sh
+++ wg_install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-echo "Installation started.."
-echo 
-echo "#####################################################"
+echo "################## Updating OS"
 yum update -y 
+echo "################## Installing WireGuard"
 sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm https://www.elrepo.org/elrepo-release-8.el8.elrepo.noarch.rpm -y 
 sudo yum install kmod-wireguard wireguard-tools -y 
-echo 
 echo "#####################################################"
-echo "installtion completed"
+echo "Installtion completed, Please reboot your machine"
