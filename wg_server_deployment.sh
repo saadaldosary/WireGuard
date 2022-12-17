88a125deb81ae28df0ac8b79b5623749e948f373
Saad Aldosary <47954648+saadaldosary@users.noreply.github.com>
2022-12-17 13:59:42 -0500

    Update wg_server_deployment.sh

--- wg_server_deployment.sh
+++ wg_server_deployment.sh
@@ -1,15 +1,17 @@
 #!/bin/bash 
 
+# Generating public and private keys 
 pub_i=$(ip -br l | awk '$1 !~ "lo|vir|wl" { print $1}')
 wg genkey | tee privatekey | wg pubkey > publickey
 private=$(cat privatekey)
 
-echo """
-[Interface]
+echo """[Interface]
 PrivateKey=$private
-Address=192.168.1.1/24
+Address= <your private network ip>
 SaveConfig=true
 PostUp=iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o $pub_i  -j MASQUERADE;
 PostUp=iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o $pub_i  -j MASQUERADE;
 ListenPort = 51820
-""" > /etc/wireguard/wg0
+""" > /etc/wireguard/wg0.conf
+# Start WireGuard
+wg-quick up wg0
