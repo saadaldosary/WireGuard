51791c7aed57da66fdacfa6c9094ddf519315dd2
Saad Aldosary <47954648+saadaldosary@users.noreply.github.com>
2022-12-17 13:53:43 -0500

    Update wg_clinet_deployment.sh

--- wg_clinet_deployment.sh
+++ wg_clinet_deployment.sh
@@ -1,18 +1,26 @@
 #!/bin/bash 
 
+# Creating private and public key. 
 pub_i=$(ip -br l | awk '$1 !~ "lo|vir|wl" { print $1}')
 wg genkey | tee privatekey | wg pubkey > publickey
 private=$(cat privatekey)
  
 echo """[Interface]
 PrivateKey=$private
-Address=192.168.1.1/24
+
+# Client Private network ip 
+Address=Client
 SaveConfig=true
 
 [Peer]
-PublicKey=p5CHpnUVHV5c61Vp2Ge58s1DxHl695PL+70dvPz/+20=
+
+PublicKey= <Wireguard Server public key >
+
+# Wireguard server:Wireguard Port 
 EndPoint=192.168.126.128:51820
+
 # configure all traffic to go through the vpn or half tunnel 
 AllowedIPs=0.0.0.0/0 """ > /etc/wireguard/wg0.conf
 
+# Start wireguard 
 wg-quick up wg0 
