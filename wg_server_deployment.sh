c8d66f656972f5ee0d0c2ac413ce4595df96782d
Saad Aldosary <47954648+saadaldosary@users.noreply.github.com>
2023-01-17 13:06:44 +0300

    Add command
    Request the user to enter a private ip for the VPN network

--- wg_server_deployment.sh
+++ wg_server_deployment.sh
@@ -4,10 +4,12 @@
 pub_i=$(ip -br l | awk '$1 !~ "lo|vir|wl" { print $1}')
 wg genkey | tee privatekey | wg pubkey > publickey
 private=$(cat privatekey)
+echo "Enter your private IP"
+read private_ip
 
 echo """[Interface]
 PrivateKey=$private
-Address= <your private network ip>
+Address= $private_ip
 SaveConfig=true
 PostUp=iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o $pub_i  -j MASQUERADE;
 PostDown=iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o $pub_i  -j MASQUERADE;
